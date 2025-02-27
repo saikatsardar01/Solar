b138c501cf1296789cd706f2c45cfc501a59fc70
sagario9 <saikatsardar180@gmail.com>
2025-02-27 23:03:34 +0530

    Added Certification

--- JS/quiz.js
+++ JS/quiz.js
@@ -48,6 +48,7 @@ function hide_modal(modal) {
   $(".overlay").hide();
 }
 
+
 function start_timer() {
   var start = new Date();
   timer = setInterval(function () {
@@ -55,27 +56,96 @@ function start_timer() {
     $(".timer").html(time);
     console.log(time);
   }, 1000);
+
+}
+
+// For USER Name
+
+// function user_name() {
+//   var userNameElement = document.getElementById('user_name');
+//   var userNameValue = userNameElement.value;
+//   console.log(userNameValue);
+// }
+
+//FOR generating certificate at the end
+
+function certificate() {
+  var canvas = document.getElementById('canvas');
+  var ctx = canvas.getContext('2d');
+  const userNameValue = document.getElementById('user_name');
+  // var downloadBtn = document.getElementById('download-btn');
+
+  const image = new Image()
+  image.src = './SLIDER/InnovationSkylineCertificate.png'
+  image.onload = function () {
+    drawImage()
+  }
+
+  function drawImage() {
+    // ctx.clearRect(0, 0, canvas.width, canvas.height)
+    ctx.drawImage(image, 0, 0, canvas.width = 1000, canvas.height = 700)
+    ctx.font = '50px system-UI'
+    ctx.fillStyle = '#fff'
+    ctx.fillText(userNameValue.value, 250, 310)
+
+    let count = 0;
+    let intervalId = setInterval(function () {
+      count++;
+      console.log("Count:", count);
+
+      if (correct == planets) {
+        clearInterval(intervalId);
+      ctx.fillText(count, 510, 500)
+
+      }
+    }, 1000);
+
+    
+  }
+
+  userNameValue.addEventListener('input', function () {
+    drawImage()
+  })
+
+
+}
+
+function dkload() {
+  var link = document.createElement('a');
+  link.href = canvas.toDataURL('image/png');
+  link.download = 'Innovation_Skyline_Certificate.png';
+  // Trigger the download
+  link.click();
 }
 
 //Start of the timer
 
-$('.st').click(function(){
+$('.st').click(function () {
   start_timer()
+  certificate()
+   // For User name
 })
-$(document).ready(function(){
+$(document).ready(function () {
   show_modal('intro');
 })
 
-$('.c_modal').click(function(){
+$('.c_modal').click(function () {
   hide_modal('modal')
 })
 
-$('.ta').click(function(){
+$('.ta').click(function () {
   hide_modal('modal');
   start_timer();
   correct = 0;
-  $(planet).css('opacity','1')
+  $(planet).css('opacity', '1')
   $('.planet_answer').hide()
   $('.answered').removeClass('answered')
-  $('.planet_holder').css('border','2px dashed rgba(255, 255, 255, 0.22)')
+  $('.planet_holder').css('border', '2px dashed rgba(255, 255, 255, 0.22)')
+})
+
+
+// Certificate
+
+$('.dwnld').click(function () {
+  dkload()
 })
